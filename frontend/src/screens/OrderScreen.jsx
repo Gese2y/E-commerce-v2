203a970fb4ba0b9374abf4852832fb44ee5b7696
gese <geseseteshome21@gmail.com>
2023-11-09 11:22:38 -0800

    add something on screen

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import './ListGroup.css';
 import {
   useDeliverOrderMutation,
   useGetOrderDetailsQuery,
@@ -187,30 +188,26 @@ const OrderScreen = () => {
               <ListGroup.Item>
                 <h2>Order Summary</h2>
               </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Items</Col>
-                  <Col>${order.itemsPrice}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Shipping</Col>
-                  <Col>${order.shippingPrice}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
+              
+    <ListGroup.Item>
+    <Row>
+      <Col>Items</Col>
+      <Col>${order.itemsPrice}</Col>
+    </Row>
+    <Row>
+      <Col>Shipping</Col>
+      <Col>${order.shippingPrice}</Col>
+    </Row>
+             
                 <Row>
                   <Col>Tax</Col>
                   <Col>${order.taxPrice}</Col>
                 </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
                 <Row>
                   <Col>Total</Col>
                   <Col>${order.totalPrice}</Col>
                 </Row>
-              </ListGroup.Item>
+    </ListGroup.Item>
               {!order.isPaid && (
                 <ListGroup.Item>
                   {loadingPay && <Loader />}
